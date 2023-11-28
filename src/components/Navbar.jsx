cacde98fc78b7dd61e2852b9e5444ca68caa172a
joeuni-ex <141595215+joeuni-ex@users.noreply.github.com>
2023-11-28 11:13:41 +0900

    Navbar 토글버튼 작동 추가

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
 import { pageLinks, socialLinks } from "../data";
 
 const Navbar = () => {
+  const [showLink, setShowLink] = useState(false);
+
+  const toggleLink = () => {
+    showLink ? setShowLink(false) : setShowLink(true);
+  };
+
+  //4초 뒤에 showLink를 false로 바꿔줌
+  useEffect(() => {
+    if (showLink) {
+      setTimeout(() => {
+        setShowLink(false);
+      }, 4000);
+    }
+  }, [showLink]);
+
   return (
     <>
       <nav className="navbar">
         <div className="nav-center">
           <div className="nav-header">
             <img src="/images/logo.svg" className="nav-logo" alt="backroads" />
-            <button type="button" className="nav-toggle" id="nav-toggle">
+            <button
+              type="button"
+              onClick={toggleLink}
+              className="nav-toggle"
+              id="nav-toggle"
+            >
               <i className="fas fa-bars"></i>
             </button>
           </div>
           {/* <!-- left this comment on purpose --> */}
-          <ul className="nav-links" id="nav-links">
+          <ul
+            className={`${showLink ? "nav-links show-links" : "nav-links"}`}
+            id="nav-links"
+          >
             {pageLinks.map((link) => {
               return (
                 <li key={link.id}>
